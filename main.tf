0bc97442dc2f6f983d845eae3d22dff5da396fdc
jdwheels <Wheeler.John.D@gmail.com>
2022-07-25 09:38:55 -0400

    Update cert-manager to v1.9.0

--- main.tf
+++ main.tf
@@ -273,7 +273,7 @@ resource "helm_release" "cert_manager" {
   ]
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
-  version          = "v1.8.2"
+  version          = "v1.9.0"
   name             = "cert-manager"
   namespace        = local.cert_manager_namespace
   create_namespace = true
