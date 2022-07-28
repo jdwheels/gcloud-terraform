5fc4da5462242c1aa87223bf7db073556692a4c5
jdwheels <Wheeler.John.D@gmail.com>
2022-07-27 20:00:17 -0400

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "url" {
   value = [for x in values(local.ingress_host_mapping) : "https://${x}"]
 }
+output "zone_dns" {
+  value = google_dns_managed_zone.zone.dns_name
+}
+output "zone_name_servers" {
+  value = google_dns_managed_zone.zone.name_servers
+}
